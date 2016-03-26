6d383e7062a19966752f010036bb9bfcfcbc8719
Pranav Jain <contact@pranavjain.me>
2016-03-26 23:48:31 +0530

    Added functionalities to the button

--- MyApplicationsNanodegree/app/src/main/java/pranav/myapplicationsnanodegree/MainActivity.java
+++ MyApplicationsNanodegree/app/src/main/java/pranav/myapplicationsnanodegree/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +28,58 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        getActionBar().setTitle("My App Portfolio");
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(MainActivity.this, "This will launch Spotify app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(MainActivity.this, "This will launch Scores app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(MainActivity.this, "This will launch Library app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(MainActivity.this, "This will launch Build it bigger app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(MainActivity.this, "This will launch XYZ Reader app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(MainActivity.this, "This will launch my own app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
